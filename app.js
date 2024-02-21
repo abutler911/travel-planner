ec3cd0e65fc2a68ff6a92cc5f4348987b5580955
Andrew Butler <abutler911@gmail.com>
2024-02-20 23:11:37 -0700

    added ejs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path"); // Include the path module
 const app = express();
 const port = 3000;
 
@@ -8,8 +9,15 @@ mongoose
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
+// Set up EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// Route for the index page
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
